734e715ded61dab0f28cfdd76ee32dd8b98a4efa
Alperen Alkan <alpernkaligrafi@gmail.com>
2025-07-07 21:27:27 +0200

    t8

--- src/AnotherClass.java
+++ src/AnotherClass.java
@@ -1,4 +1,6 @@
 public class AnotherClass {
    //Remote version r1
   public String string1= "Biri sen, biri ben, iki damla yas akti gozlerimden";
+  //t8
+  public String string2 ="Parildayip duran insanlara bak";
 }
